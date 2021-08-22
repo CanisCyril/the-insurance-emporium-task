69dd99e7962cd2f0aa1bb8c28b3ed00f22538df8
CanisCyril <34339556+CanisCyril@users.noreply.github.com>
2021-08-22 09:42:28 +0100

    built export
    export is showing correct data

---
+++ app/Console/Commands/TheInsuranceEmporium/SalaryExport.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands\TheInsuranceEmporium;
+
+use Illuminate\Console\Command;
+use App\Exports\SalaryExportData;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Carbon;
+
+class SalaryExport extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'salary:export';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Exports a CSV file of employee salaries';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		$this->export();
+	}
+
+	public function export() 
+	{
+
+		$filename = 'exports/salary-export-' . Carbon::now()->year . '.csv';
+
+		return Excel::store(new SalaryExportData, $filename);
+
+	}
+}
---
+++ app/Exports/SalaryExportData.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Carbon;
+
+class SalaryExportData implements FromCollection, WithHeadings
+{
+    // public $results;
+
+    // public function __construct(array $results)
+    // {
+    //     $this->results = $this->periodAndBasicPaymentDates();
+    // }
+
+    public function collection()
+    {
+
+       return $results = $this->periodAndBasicPaymentDates();
+    }
+
+    public function headings(): array {
+        return [
+            "Period", "Basic Pay Date", "Bonus Pay Date"
+        ];
+    }
+
+    public function periodAndBasicPaymentDates()
+	{
+		$lastDatesOfMonths = [];
+		$paymentDates = [];
+
+		$date = Carbon::parse(Carbon::now('UTC'))->format('Y-m-d'); //Carbon parsed is used each time format is requred, format converts it to string, functions cannot be used on a string
+
+		for ($x = 0; $x <= 12; $x++) { //loop 12 times 
+			if($x > 0) { //Skip first iteration 
+		   		$date = Carbon::parse($date)->addMonthsNoOverflow(1)->endOfMonth(); //Get next month of date, then convert it to the last day of the month.
+				
+			} else {
+				$date = Carbon::parse($date)->endOfMonth();
+			}
+
+			$date = Carbon::parse($date)->format('Y-m-d'); //Converting to string date format from Carbon
+
+			$lastDatesOfMonths[] = $date; //Push date to array
+		}
+
+		foreach($lastDatesOfMonths as $lastWeekDay) { //Iterate through array that dates were added to
+			$loopStopper = 0;
+
+			$lastWeekDay = Carbon::parse($lastWeekDay); //Convert back to Carbon
+
+			while($loopStopper === 0) { //Keep looping until condition changes
+				if($lastWeekDay->dayOfWeek === Carbon::SUNDAY) { //If date is a Sunday, remove a day
+					$lastWeekDay->subDays(1);
+				} else if ($lastWeekDay->dayOfWeek === Carbon::SATURDAY) { //If date is a Saturday, remove a day
+					$lastWeekDay->subDays(1);
+				} else { //Once the date is a working day push in to a new array
+
+					$obj = new \stdClass();
+
+					$obj->periodDate = $lastWeekDay->format('m-Y');
+					$obj->basicPayDate = $lastWeekDay->format('Y-m-d');
+					array_push($paymentDates, $obj);
+					$loopStopper = 1; //Stop loop for each iteration
+				}
+			}
+		}
+		$results = $this->bonusPaymentDates($paymentDates);
+        return $results;
+	}
+
+	public function bonusPaymentDates($paymentDates)
+	{
+
+		//This function is fairly similar to periodAndBasicPaymentDates()
+
+		date_default_timezone_set('Europe/London'); //set timezone
+
+		$lastDatesOfMonths = [];
+
+		$date = Carbon::parse('10th' . date('M'))->format('Y-m-d');
+
+
+		for ($x = 0; $x <= 12; $x++) { 
+			if($x > 0) { 
+		   		$date = Carbon::parse($date)->addMonthsNoOverflow(1); 
+				
+			} else {
+				$date = Carbon::parse($date);
+			}
+
+			$date = Carbon::parse($date)->format('Y-m-d'); 
+
+			$lastDatesOfMonths[] = $date; 
+
+			foreach($lastDatesOfMonths as $lastWeekDay) { //add days after 10th if it
+				$loopStopper = 0;
+
+				$lastWeekDay = Carbon::parse($lastWeekDay); 
+
+				while($loopStopper === 0) { 
+					if($lastWeekDay->dayOfWeek === Carbon::SUNDAY) {
+						$lastWeekDay->addDays(1); 
+					} else if ($lastWeekDay->dayOfWeek === Carbon::SATURDAY) {
+						$lastWeekDay->addDays(1);
+					} else { 
+
+						$paymentDates[$x]->bonusPayDate = $lastWeekDay->format('Y-m-d');
+
+						$loopStopper = 1; 
+					}
+				}
+			}
+		}
+
+        return collect($paymentDates);
+	}
+}
